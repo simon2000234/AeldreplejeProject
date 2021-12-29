dad1145086012934e1a253f50e6368b9e099de4c
Simon <Warcraft.simon@gmail.com>
2021-12-29 10:31:47 +0100

    specflow

---
+++ Spec4TestExam/Features/Notification.feature
@@ -0,0 +1,19 @@
+﻿Feature: Notification
+	In order to avoid overworking
+	As a user
+	I want to be told when i try to take a shift that i cant take
+
+@mytag
+Scenario: Taking a shift im unqualified for
+	Given i have a qualification level of 2
+	And the shift has as qualification level of 3
+	When i check qualification
+	Then i should be rejected
+
+Scenario: Taking a shift im qualified for
+    Given i have a qualification level of 3
+    And the shift has as qualification level of 3
+    When i check qualification
+    Then i should be accepted
+
+
---
+++ Spec4TestExam/Steps/NotificationSteps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using AeldreplejeCore.Core.Application.Impl;
+using AeldreplejeCore.Core.Entity;
+using TechTalk.SpecFlow;
+using Xunit;
+
+
+namespace Spec4TestExam.Steps
+{
+    [Binding]
+    public class NotificationSteps
+    {
+        private NotificationService ns;
+        private User currentUser;
+        private PendingShift currentShift;
+        private bool result;
+        public NotificationSteps()
+        {
+            ns = new NotificationService();
+        }
+
+        [Given(@"i have a qualification level of (.*)")]
+        public void GivenIHaveAQualificationLevelOf(int p0)
+        {
+            Group group = new() {QualificationNumber = p0};
+            currentUser = new User() {Group = group};
+        }
+        
+        [Given(@"the shift has as qualification level of (.*)")]
+        public void GivenTheShiftHasAsQualificationLevelOf(int p0)
+        {
+            Shift shift = new() {ShiftQualificationNumber = p0};
+            currentShift = new PendingShift() {Shift = shift};
+        }
+
+        [When(@"i check qualification")]
+        public void WhenICheckQualification()
+        {
+            result = ns.IsUserQualified(currentShift, currentUser);
+        }
+
+
+        [Then(@"i should be rejected")]
+        public void ThenIShouldBeRejected()
+        {
+            Assert.False(result);
+        }
+        
+        [Then(@"i should be accepted")]
+        public void ThenIShouldBeAccepted()
+        {
+            Assert.True(result);
+        }
+    }
+}
